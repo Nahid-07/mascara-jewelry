942d0717d29e0a1b2a584544ea0ab8b7c6fab4ca
Nahid-07 <nahidasif1998@gmail.com>
2023-02-11 12:39:05 +0600

    section4 is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './header/Header';
 import Section3 from './section-3/Section3';
 import Secttion from './section1/Secttion';
 import Section2 from './section2/Section2';
+import Section4 from './section4/Section4';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Secttion></Secttion>
     <Section2></Section2>
     <Section3></Section3>
+    <Section4></Section4>
     </div>
   );
 }
---
+++ src/section4/Section4.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Section4 = () => {
+    return (
+        <section className='bg-[#d9d4c8] text-center py-7 px-12 lg:px-0'>
+            <h4 className='text-base text-[#846315]'>OUR MISSION IS SIMPLE</h4>
+            <p className='text-2xl text-[#575348] font-semibold'>Our goal is to create beautiful and traditional high quality jewelry to make you sparkle.</p>
+        </section>
+    );
+};
+
+export default Section4;
