2d4f0b199b3e551276d0204ce127ca1f63e5df08
Nahid-07 <nahidasif1998@gmail.com>
2023-03-14 23:22:45 +0600

    Privet route implemented

---
+++ src/routes/PrivetRoute.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import Speener from '../component/Speener';
+import { AuthContext } from '../context/ContextAuth';
+
+const PrivetRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext);
+    const location = useLocation()
+    if(loading){
+        return <Speener></Speener>
+    }
+    if(user && user.uid){
+        return children
+    }
+    <Navigate to='/login' state={{ from: location }} replace></Navigate>
+};
+
+export default PrivetRoute;
