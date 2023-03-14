5261beec2b0dbac7bf64230da859045f586e5dea
Nahid-07 <nahidasif1998@gmail.com>
2023-03-14 10:24:33 +0600

    navbar modified

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const menus = [
+  const leftMenu = [
     { name: "Shop", path: "/shop" },
     { name: "Gifts", path: "/gifts" },
     { name: "Engagement", path: "/engagement" },
     { name: "About", path: "/about" },
-    { name: "Dashboard", path: "/dashboard" },
   ];
   const [open, setOpen] = useState(false);
   const activeClass =
@@ -19,7 +18,7 @@ const Header = () => {
           <nav>
             <ul className="flex space-x-5">
               {
-                menus.map(menu => <li className={activeClass}><Link to={menu.path}>{menu.name}</Link></li>)
+                leftMenu.map(menu => <li className={activeClass}><Link to={menu.path}>{menu.name}</Link></li>)
               }
 
             </ul>
@@ -30,79 +29,10 @@ const Header = () => {
           <p>REFIND JEWELRY</p>
         </div>
         <div className="w-[30%]">
-          <ul className="flex space-x-5 justify-end">
-            <li>
-              <a href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                  />
-                </svg>
-              </a>
-            </li>
+          <ul className="flex space-x-5 justify-end items-center">
+            <li className={activeClass}><Link to='/dashboard'>Dashboard</Link></li>
+            <li><button className="border border-black px-3 py-1 rounded hover:bg-black hover:text-white duration-500">Login</button></li>
+            <li><button className="border border-black px-3 py-1 rounded hover:bg-black hover:text-white duration-500">Logout</button></li>
           </ul>
         </div>
       </div>
@@ -139,7 +69,7 @@ const Header = () => {
       >
         <ul className="flex flex-col items-center  space-y-5">
           {
-            menus.map(menu =><li className={activeClass}><Link to={menu.path}>{menu.name}</Link></li> )
+            leftMenu.map(menu =><li className={activeClass}><Link to={menu.path}>{menu.name}</Link></li> )
           }
         </ul>
         <ul className="flex space-x-5 items-center justify-center mt-5">
