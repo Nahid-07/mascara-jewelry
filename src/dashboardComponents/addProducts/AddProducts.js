aa72396fc0c661d3030c4034e8cb2be783e59ee7
Nahid-07 <nahidasif1998@gmail.com>
2023-03-16 00:57:48 +0600

    add product functionality added

--- src/dashboardComponents/addProducts/AddProducts.js
+++ src/dashboardComponents/addProducts/AddProducts.js
@@ -1,10 +1,48 @@
 import React from "react";
+import { toast } from "react-hot-toast";
+import SmallSpinner from "../../component/SmallSpeener";
+
 
 const AddProducts = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const image = e.target.image.files[0];
+    const category = e.target.category.value;
+    const price = e.target.price.value;
+    const description = e.target.description.value;
+    const formData = new FormData();
+    formData.append("image", image);
+    const url =
+      "https://api.imgbb.com/1/upload?key=afef7a18a343b614ef922e54a0d39132";
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const photoUrl = data.data.display_url;
+        fetch("http://localhost:5000/add-product", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            category,
+            price,
+            description,
+            photoUrl,
+          }),
+        }).then(()=>{
+            toast.success("successfully added to the database");  
+        })
+      });
+  };
   return (
     <div className="flex flex-col justify-center items-center p-20">
       <h1 className="text-2xl font-bold">Add a Product</h1>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div className="">
           <label htmlFor="title">Title</label>
           <input
@@ -13,6 +51,7 @@ const AddProducts = () => {
             className="border-gray-300 border-2 focus:outline-blue-400 w-80 md:w-96 block mb-5 px-3 py-2 rounded"
             type="text"
             placeholder="Enter product title"
+            required
           />
           <label htmlFor="file">Upload Image</label>
           <input
@@ -20,6 +59,7 @@ const AddProducts = () => {
             className="border-gray-300 border-2 focus:outline-blue-400 w-80 md:w-96 block mb-5 px-3 py-2 rounded"
             type="file"
             accept="image"
+            required
           />
         </div>
         <div className="">
@@ -38,6 +78,7 @@ const AddProducts = () => {
             className="border-gray-300 border-2 focus:outline-blue-400 w-80 md:w-96 block mb-5 px-3 py-2 rounded"
             type="text"
             placeholder="$100"
+            required
           />
           <div className="flex flex-col">
             <label htmlFor="desc">Description</label>
@@ -47,6 +88,7 @@ const AddProducts = () => {
               rows="3"
               className="px-3 py-2 md:w-96 w-80 border-gray-300 border-2 focus:outline-blue-400"
               placeholder="Write short description"
+              required
             ></textarea>
           </div>
         </div>
